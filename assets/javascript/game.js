4e7cf3e918438fc1ea5a055d91e68af125e8d8ed
Jessica Rubio Lachira <jrubi89@icloud.com>
2020-04-25 21:24:29 -0400

    Game conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,11 @@ var winCounter = 0;
 var lossCounter = 0;
 var targetScore = 0;
 var crystalBtn = $('.crystalButton');
+
+//Game conditions.
+function startGame(){
+    currentScore = 0;
+    $('#scoreBox').text(currentScore);
+    $('.alert').addClass('modal');
+    $('#wins').text(winCounter);
+    $('#losses').text(lossCounter);
