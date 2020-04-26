a8d541e53e56391518537133bd7fa166a7f5532b
Jessica Rubio Lachira <jrubi89@icloud.com>
2020-04-25 21:51:22 -0400

    Random Number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,26 @@ var crystalBtn = $('.crystalButton');
 
 //Game conditions.
 function startGame(){
-    currentScore = 0;
-    $('#scoreBox').text(currentScore);
-    $('.alert').addClass('modal');
-    $('#wins').text(winCounter);
-    $('#losses').text(lossCounter);
+currentScore = 0;
+$('#scoreBox').text(currentScore);
+$('.alert').addClass('modal');
+$('#wins').text(winCounter);
+ $('#losses').text(lossCounter);
+
+ // Random number generates at start of game
+ targetScore = Math.floor(Math.random() * (120 - 19 +1)  + 19);
+ console.log("target score is", targetScore);
+ $('#scoreTarget').text(targetScore);
+
+// Fill Arr: have crystals generate random number b/t 1 and 12
+ var buttonValues = [];
+ for (var i = 0; i < 4; i++) {
+    buttonValues.push(Math.floor(Math.random() * (12 - 1) +1));
+    console.log(buttonValues);
+}
+$('#button1').attr('value', buttonValues[0]);
+$('#button2').attr('value', buttonValues[1]);
+$('#button3').attr('value', buttonValues[2]);
+$('#button4').attr('value', buttonValues[3]); 
+}
+startGame();
